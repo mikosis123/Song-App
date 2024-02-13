142a708dd584605476a1d1f22ecd7aa8632df227
mikosis123 <mikosis93@gmail.com>
2024-02-13 22:57:59 +0300

    create connection to mongoose

--- Server/index.js
+++ Server/index.js
@@ -3,11 +3,17 @@ const mongoose = require("mongoose");
 const corse = require("cors");
 require("dotenv").config();
 const app = express();
+const router = require("../Server/routes/Taskroutes");
 app.use(express.json());
 app.use(corse());
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log(err));
+app.use("/api", router);
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
 });
