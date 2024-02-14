1f927b0d4d50398c56436c2127587389e9f8576d
mikosis123 <mikosis93@gmail.com>
2024-02-14 00:06:35 -0800

    add task function

--- Server/Taskcontrols/Taskcontrollers.js
+++ Server/Taskcontrols/Taskcontrollers.js
@@ -1,6 +1,17 @@
 const Songmodel = require("./Songsmodel");
 module.exports.Getsongs = async (req, res) => {
-  //   const songs = await Songmodel.find();
-  //   res.send(songs);
-  res.send("hiiiiiii");
+  const songs = await Songmodel.find();
+  res.send(songs);
+};
+module.exports.Postsongs = async (req, res) => {
+  const { task } = req.body;
+  Songmodel.create({ task })
+    .then(
+      (data) => console.log("Data has been inserted successfully"),
+      res.status(200).send(data)
+    )
+    .catch(
+      (err) => console.log("Data has not been inserted successfully"),
+      res.send({ error: err, msg: "Data has not been inserted successfully" })
+    );
 };
