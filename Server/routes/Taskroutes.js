85c8264d7ac81235bf3dc18b753a5f20e2838e2c
mikosis123 <mikosis93@gmail.com>
2024-02-14 00:23:12 -0800

    importing the task

--- Server/routes/Taskroutes.js
+++ Server/routes/Taskroutes.js
@@ -1,5 +1,10 @@
 const { Router } = require("express");
 const router = Router();
-const { Getsongs } = require("../Taskcontrols/Taskcontrollers");
+const {
+  Getsongs,
+  Postsongs,
+  Deletesongs,
+  Updatesongs,
+} = require("../Taskcontrols/Taskcontrollers");
 router.get("/get", Getsongs);
 module.exports = router;
