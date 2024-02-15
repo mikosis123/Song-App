fa4e7d6096f5e4fb04628ad341868ba8587cdcaa
mikosis123 <mikosis93@gmail.com>
2024-02-15 13:39:35 +0300

    songs list ui

--- Client/songapp/src/Components/Songs.tsx
+++ Client/songapp/src/Components/Songs.tsx
@@ -1,7 +1,112 @@
 import React from "react";
+import TableContaints from "./TableContaints";
 
 const Songs = () => {
-  return <div className="bg-white w-full h-screen"></div>;
+  return (
+    <div className="bg-white w-full h-screen">
+      <div className="bg-blue-500 h-[300px]"></div>
+      <div className="relative  shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left  text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Title
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Artists
+                  <a href="#">
+                    <svg
+                      className="w-3 h-3 ms-1.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  album
+                  <a href="#">
+                    <svg
+                      className="w-3 h-3 ms-1.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Genre
+                  <a href="#">
+                    <svg
+                      className="w-3 h-3 ms-1.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="">
+            <TableContaints
+              itemName="Apple MacBook Pro 17"
+              color="Silver"
+              category="Laptop"
+              price={2999}
+              onEditClick={() => {}} // Pass the onEditClick handler
+            />
+
+            <TableContaints
+              itemName="Apple MacBook Pro 17"
+              color="Silver"
+              category="Laptop"
+              price={2999}
+              onEditClick={() => {}} // Pass the onEditClick handler
+            />
+            <TableContaints
+              itemName="Apple MacBook Pro 17"
+              color="Silver"
+              category="Laptop"
+              price={2999}
+              onEditClick={() => {}} // Pass the onEditClick handler
+            />
+            <TableContaints
+              itemName="Apple MacBook Pro 17"
+              color="Silver"
+              category="Laptop"
+              price={2999}
+              onEditClick={() => {}} // Pass the onEditClick handler
+            />
+            <TableContaints
+              itemName="Apple MacBook Pro 17"
+              color="Silver"
+              category="Laptop"
+              price={2999}
+              onEditClick={() => {}} // Pass the onEditClick handler
+            />
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Songs;
