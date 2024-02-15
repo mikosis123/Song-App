18d63f414dfde7bb873d9995e2bf7c51015c9664
mikosis123 <mikosis93@gmail.com>
2024-02-15 13:38:49 +0300

    routing set up

--- Client/songapp/src/App.tsx
+++ Client/songapp/src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./Components/Sidebar";
 import Songs from "./Components/Songs";
+import Artists from "./Components/Artists";
+import Geners from "./Components/Geners";
 
 function App() {
   return (
@@ -11,7 +14,15 @@ function App() {
         <Sidebar />
       </div>
       <div className="w-[72%] h-[90%] mt-8 ">
-        <Songs />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Songs />}>
+              <Route index element={<Songs />} />
+              <Route path="blogs" element={<Artists />} />
+              <Route path="contact" element={<Geners />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
