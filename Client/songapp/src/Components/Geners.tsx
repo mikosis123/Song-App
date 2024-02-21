3f2643923b3e82927c551f1132b5ea133d41222f
mikosis123 <mikosis93@gmail.com>
2024-02-21 22:50:39 +0300

    gener edit

--- Client/songapp/src/Components/Geners.tsx
+++ Client/songapp/src/Components/Geners.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Showcard from "./Showcard";
+import GenerCard from "./GenerCard";
+import { useGetSongQuery } from "../Features/song.api";
 
 const Geners = () => {
+  const { data: gener } = useGetSongQuery(undefined);
+
   return (
-    <div className="flex justify-centern items-center h-screen w-full">
-      <Showcard />
+    <div className="bg-gradient-to-b h-screen w-full overflow-hidden overflow-x-auto from-gray-300 to-blue-400 ">
+      <div>
+        <h1 className=" flex justify-center items-center text-3xl mt-24">
+          Geners
+        </h1>
+      </div>
+      <div className="flex gap-4 flex-wrap justify-center items-center mt-32  w-full">
+        {gener?.map((item: any) => {
+          return <GenerCard key={item.id} gener={item.Genre} />;
+        })}
+      </div>
     </div>
   );
 };
