68ba518517039213365984483a3626266b1e5916
mikosis123 <mikosis93@gmail.com>
2024-02-21 15:47:29 +0300

    change the width

--- Client/songapp/src/App.tsx
+++ Client/songapp/src/App.tsx
@@ -11,10 +11,10 @@ import Albums from "./Components/Albums";
 function App() {
   return (
     <div className="flex justify-start w-full bg-gradient-to-r from-gray-300 to-gray-200">
-      <div className="min-h-screen relative mr-[40px] flex justify-between align-center ">
+      <div className="min-h-screen relative  flex justify-between align-center ">
         <Sidebar />
       </div>
-      <div className="w-[72%] h-[90%]  ">
+      <div className="w-[80%] h-[90%]  ">
         <Routes>
           <Route path="/Artists" element={<Artists />} />
           <Route path="/Songs" element={<Songs />} />
