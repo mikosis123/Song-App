1e1f7ead32ed9acd088b66fc41baab1462a71108
mikosis123 <mikosis93@gmail.com>
2024-03-02 20:08:24 +0300

    adding upload song in main route

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 import routes from "./routes/Taskroutes.js";
+import cloudinaryUploader from "./Songuploads/cloudinaryUploader.js";
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(corse());
 app.get("/", (req, res) => {
@@ -15,6 +16,20 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   res.send("Server is running");
+});
+
+app.post("/uploadAudio", upload, async (req, res) => {
+  // check for any file validation errors from multer
+  if (req.fileValidationError) {
+    return res
+      .status(400)
+      .json({ message: `File validation error: ${req.fileValidationError}` });
+  }
+  //   invoke the uplader function to handle the upload to cloudinary
+  //   we are passing the req, and res to cloudinaryUploader function
+  const audioResponse = await cloudinaryUploader(req, res);
+  //   send response with audio response from cloudinary
+  return res.status(200).json({ audioResponse: audioResponse.secure_url });
 });
 
 mongoose
