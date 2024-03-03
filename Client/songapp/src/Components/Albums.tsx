07e56eb8f38da15ec2bfa5d94df8a6eb52e3e5eb
mikosis123 <mikosis93@gmail.com>
2024-03-03 17:02:43 +0300

    album filter

--- Client/songapp/src/Components/Albums.tsx
+++ Client/songapp/src/Components/Albums.tsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AlbumCard from "./AlbumCard";
 import { useGetSongQuery } from "../Features/song.api";
 
+interface AlnumSongCounts {
+  [key: string]: number;
+}
+
 const Albums = () => {
-  const { data: album } = useGetSongQuery(undefined);
+  const { data: songs } = useGetSongQuery(undefined);
+  const [albumSongCounts, setalbumSongCounts] = useState<AlnumSongCounts>({});
+
+  useEffect(() => {
+    const countSongsByArtist = () => {
+      const counts: AlnumSongCounts = {};
+
+      if (songs) {
+        songs.forEach((song: any) => {
+          const Album = song.Album;
+          counts[Album] = counts[Album] ? counts[Album] + 1 : 1;
+        });
+      }
+      return counts;
+    };
+
+    setalbumSongCounts(countSongsByArtist());
+  }, [songs]);
+
+  // Filter out duplicate artists
+  const uniqueArtists = Object.keys(albumSongCounts);
 
   return (
-    <div className="bg-gradient-to-b overflow-hidden overflow-x-auto from-gray-300 to-blue-400 flex gap-4 flex-wrap justify-center items-center h-screen w-full">
-      {album?.map((item: any) => {
-        return <AlbumCard key={item.id} album={item.Album} />;
+    <div className="bg-gradient-to-b  overflow-y-auto from-gray-300 to-blue-400 flex gap-4 flex-wrap justify-center items-center h-screen w-full">
+      {uniqueArtists?.map((Album: any) => {
+        return (
+          <AlbumCard
+            key={Album}
+            album={Album}
+            counts={albumSongCounts[Album]}
+          />
+        );
       })}
     </div>
   );
