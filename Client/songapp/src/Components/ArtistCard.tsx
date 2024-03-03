b6154e5cf3ba90190bc410f54a75ab1e65b2adba
mikosis123 <mikosis93@gmail.com>
2024-03-03 23:21:39 +0300

    artistcard style change

--- Client/songapp/src/Components/ArtistCard.tsx
+++ Client/songapp/src/Components/ArtistCard.tsx
@@ -7,27 +7,23 @@ type artistcard = {
 
 const ArtistCard = ({ artist, songCount }: artistcard) => {
   return (
-    <div>
-      <div className="w-full max-w-sm bg-blue-300 shadow-lg border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="shadow-xl">
+      <div className="w-full max-w-sm bg-[#e5e5e5] shadow-lg border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
           <img className="p-8 rounded-t-lg" src={image} alt="product image" />
         </a>
         <div className="px-5 pb-5">
           <a href="#">
             <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              name of the Artists is : {artist}
+              Artists :{" "}
+              <span className="text-3xl font-bold text-blue-600">{artist}</span>
             </h5>
           </a>
-          <div className="flex items-center mt-2.5 mb-5">
-            <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-              total number of songs
+
+          <div className="flex items-center justify-end">
+            <span className="text-xl font-bold text-gray-900 dark:text-white">
+              <span>{songCount}</span> songs
             </span>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              total number of songs
-            </span>
-            <span>{songCount}</span>
           </div>
         </div>
       </div>
