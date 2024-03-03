44ae23ffb328dfde2b136a807f015c39df5f1fc4
mikosis123 <mikosis93@gmail.com>
2024-03-03 14:39:03 +0300

    image upload handling

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,8 @@
 import upload from "./Songuploads/multer.js";
 import uploader from "./Songuploads/cloudinaryUploader.js";
+import uploadImage from "./Songuploads/ImageMulter.js";
 import express from "express";
+
 import mongoose from "mongoose";
 import corse from "cors";
 import dotenv from "dotenv";
@@ -8,6 +10,7 @@ dotenv.config();
 const app = express();
 import routes from "./routes/Taskroutes.js";
 import cloudinaryUploader from "./Songuploads/cloudinaryUploader.js";
+import cloudinaryImage from "./Songuploads/cloudinaryImage.js";
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(corse());
 app.get("/", (req, res) => {
@@ -30,6 +33,24 @@ app.post("/uploadAudio", upload, async (req, res) => {
   const audioResponse = await cloudinaryUploader(req, res);
   //   send response with audio response from cloudinary
   return res.status(200).json({ audioResponse: audioResponse.secure_url });
+});
+
+app.post("/uploadImage", uploadImage, async (req, res) => {
+  if (req.fileValidationError) {
+    return res
+      .status(400)
+      .json({ message: `File validation error: ${req.fileValidationError}` });
+  }
+
+  try {
+    // invoke the uploader function to handle the upload to cloudinary
+    const imageResponse = await cloudinaryImage(req, res);
+    // send response with image response from cloudinary
+    return res.status(200).json({ imageResponse: imageResponse.secure_url });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error uploading image" });
+  }
 });
 
 mongoose
