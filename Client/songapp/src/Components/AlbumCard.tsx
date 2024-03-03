d354ea9e7c1f714e5e52ff77b38f461b04c914ff
mikosis123 <mikosis93@gmail.com>
2024-03-03 23:20:50 +0300

    style change

--- Client/songapp/src/Components/AlbumCard.tsx
+++ Client/songapp/src/Components/AlbumCard.tsx
@@ -8,23 +8,23 @@ type artistcard = {
 
 const AlbumCard = ({ album, counts }: artistcard) => {
   return (
-    <div>
-      <div className="w-full max-w-sm bg-blue-300 shadow-lg border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="shadow-xl">
+      <div className="w-full max-w-sm bg-[#e5e5e5] shadow-lg border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <img className="p-8 rounded-t-lg" src={image} alt="product image" />
+          <img className="p-8 rounded-lg " src={image} alt="product image" />
         </a>
         <div className="px-5 pb-5">
           <a href="#">
             <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              name of the album is : {album}
+              <span className="text-3xl font-bold text-blue-600"> {album}</span>{" "}
+              album
             </h5>
           </a>
         </div>
-        <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">
-            total number of songs
+        <div className="flex items-center justify-end px-5 pb-5">
+          <span className="text-xl font-bold flex flex-end text-gray-500 dark:text-white">
+            {counts} songs
           </span>
-          <span>{counts}</span>
         </div>
       </div>
     </div>
