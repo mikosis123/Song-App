aa39a71bc91dc5881c2967e4f12eeb02a3855ca4
mikosis123 <mikosis93@gmail.com>
2024-03-03 17:03:41 +0300

    geners filter

--- Client/songapp/src/Components/Geners.tsx
+++ Client/songapp/src/Components/Geners.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Showcard from "./Showcard";
 import GenerCard from "./GenerCard";
 import { useGetSongQuery } from "../Features/song.api";
-
+interface generSongCounts {
+  [key: string]: number;
+}
 const Geners = () => {
-  const { data: gener } = useGetSongQuery(undefined);
+  const { data: songs } = useGetSongQuery(undefined);
+  const [generSongCounts, setgenerSongCounts] = useState<generSongCounts>({});
+
+  useEffect(() => {
+    const countSongsByArtist = () => {
+      const counts: generSongCounts = {};
+
+      if (songs) {
+        songs.forEach((song: any) => {
+          const Genre = song.Genre;
+          counts[Genre] = counts[Genre] ? counts[Genre] + 1 : 1;
+        });
+      }
+      return counts;
+    };
+
+    setgenerSongCounts(countSongsByArtist());
+  }, [songs]);
+
+  // Filter out duplicate artists
+  const uniqueArtists = Object.keys(generSongCounts);
 
   return (
     <div className="bg-gradient-to-b h-screen w-full overflow-hidden overflow-x-auto from-gray-300 to-blue-400 ">
@@ -14,8 +36,14 @@ const Geners = () => {
         </h1>
       </div>
       <div className="flex gap-4 flex-wrap justify-center items-center mt-32  w-full">
-        {gener?.map((item: any) => {
-          return <GenerCard key={item.id} gener={item.Genre} />;
+        {uniqueArtists?.map((Genre: any) => {
+          return (
+            <GenerCard
+              key={Genre}
+              gener={Genre}
+              counts={generSongCounts[Genre]}
+            />
+          );
         })}
       </div>
     </div>
