cfae605f7bae2511d566933566cc529178d4a7ff
mikosis123 <mikosis93@gmail.com>
2024-03-04 11:17:07 +0300

    image import from server data

--- Client/songapp/src/Components/AlbumCard.tsx
+++ Client/songapp/src/Components/AlbumCard.tsx
@@ -1,5 +1,7 @@
 import { count } from "console";
 import image from "../Components/pexels-elviss-railijs-bitāns-1389429.jpg";
+import { useEffect, useState } from "react";
+import { useGetSongQuery } from "../Features/song.api";
 
 type artistcard = {
   album: string;
@@ -7,11 +9,31 @@ type artistcard = {
 };
 
 const AlbumCard = ({ album, counts }: artistcard) => {
+  const [imageSource, setImageSource] = useState<string | null>(null);
+  const { data: songs } = useGetSongQuery(undefined);
+
+  useEffect(() => {
+    const fetchImageSource = async () => {
+      if (songs) {
+        const artistSongs = songs.filter((song: any) => song.Album === album);
+        if (artistSongs.length > 0) {
+          // Assuming each song object contains an 'imageURL' property
+          setImageSource(artistSongs[0].Imagefile);
+        }
+      }
+    };
+
+    fetchImageSource();
+  }, [album, songs]);
   return (
     <div className="shadow-xl">
       <div className="w-full max-w-sm bg-[#e5e5e5] shadow-lg border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <img className="p-8 rounded-lg " src={image} alt="product image" />
+          <img
+            className="p-8 rounded-lg "
+            src={imageSource || image}
+            alt="product image"
+          />
         </a>
         <div className="px-5 pb-5">
           <a href="#">
