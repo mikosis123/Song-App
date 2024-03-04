e231ffc4052f8a6d5ac6b0cfacf6c08f0a90f65d
mikosis123 <mikosis93@gmail.com>
2024-03-04 23:14:28 +0300

    title position change

--- Client/src/Components/TableContaints.tsx
+++ Client/src/Components/TableContaints.tsx
@@ -119,7 +119,7 @@ const TableContaints = ({
         {Title}
         {isEditing && (
           <input
-            className="bg-gray-50 mx-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  w-[150px] ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 mx-4 mt-8 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  w-[150px] ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             type="text"
             name="Title"
             value={formData.Title}
@@ -137,9 +137,9 @@ const TableContaints = ({
         }}
       >
         {play ? (
-          <FaPlayCircle className="text-3xl" />
+          <FaPauseCircle className="text-3xl text-blue-500" />
         ) : (
-          <FaPauseCircle className="text-3xl" />
+          <FaPlayCircle className="text-3xl" />
         )}
         <audio ref={audioRef} src={audioUrl} />
       </th>
